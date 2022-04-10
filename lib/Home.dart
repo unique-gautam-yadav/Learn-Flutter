8c981e528bafd4289218a0708f20ebb06780e72b
Gautamyadavglaian <gautam.gla_csd20@gla.ac.in>
2022-04-11 05:29:26 +0530

    First Commit

---
+++ lib/Home.dart
@@ -0,0 +1,23 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gautam Yadav"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome To My Application"),
+        ),
+      ),
+      drawer: Drawer(
+        child: ,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:first_app/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
